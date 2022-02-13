a05de403dbfd3880993dc82285792ee9b2218b99
zajus <zajus@poczta.fm>
2022-02-13 16:58:40 +0100

    Add Episode models

---
+++ app/src/main/java/com/example/android_ca/feature/data/remote/model/EpisodeRemote.kt
@@ -0,0 +1,24 @@
+package com.example.android_ca.feature.data.remote.model
+
+import com.example.android_ca.feature.episodes.domain.model.Episode
+import com.google.gson.annotations.SerializedName
+
+
+class EpisodeRemote(
+    @SerializedName("id") val id: Int,
+    @SerializedName("name") val name: String,
+    @SerializedName("air_date") val airDate: String,
+    @SerializedName("episode") val code: String,
+    @SerializedName("characters") val characters: List<String>,
+    @SerializedName("url") val url: String,
+    @SerializedName("created") val created: String
+) {
+    fun toEpisode() = Episode(
+        id = id,
+        name = name,
+        airDate = airDate,
+        code = code,
+        characters = characters,
+        url = url
+    )
+}
